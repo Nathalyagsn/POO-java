837d984f50f0dc1079ad1e8f40eea3b43178fa52
Nathalya <nathalya.gsn@gmail.com>
2024-07-15 15:43:15 -0300

    Aula04

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,15 @@
+package aula04;
+
+public class Aula04 {
+
+   
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Nic","Verde", 0.4f);
+        c1.status();
+        System.out.println("==============================");
+        Caneta c2 = new Caneta("Faber Castell", "Rosa", 0.5f);
+        c2.status();
+        
+    }
+
+}
