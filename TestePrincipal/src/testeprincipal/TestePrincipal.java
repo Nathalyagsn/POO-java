ae55dd50c6273cfc65e2087fecf7035fd1ab50ac
Nathalya <nathalya.gsn@gmail.com>
2024-07-31 16:08:05 -0300

    primeiro commit após formatar

---
+++ TestePrincipal/src/testeprincipal/TestePrincipal.java
@@ -0,0 +1,13 @@
+
+
+package testeprincipal;
+
+
+public class TestePrincipal {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Olá, teste após formatar. Testanto também o envio para o GitHub");
+    }
+
+}
