647debd5d12ed37c78a85bc10599492e50148857
Nathalya <nathalya.gsn@gmail.com>
2024-08-20 12:11:50 -0300

    aula13

---
+++ Aula13/src/aula13/Aula13.java
@@ -0,0 +1,9 @@
+package aula13;
+public class Aula13 {
+    public static void main(String[] args) {
+        Cachorro C = new Cachorro();
+        C.reagir(2,12.5f);
+        C.reagir(17,4.5f);
+    }
+
+}
